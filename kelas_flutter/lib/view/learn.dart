5b48a2f1a9dbcd14a335c4b3714261ea0c25bb9b
bagoelz <mpmahesaputra@gmail.com>
2023-09-20 16:55:04 +0700

    update image

--- kelas_flutter/lib/view/learn.dart
+++ kelas_flutter/lib/view/learn.dart
@@ -10,6 +10,7 @@ class LearnWidget extends StatelessWidget {
       LearnModel(
           title: "Design System UI/UX design",
           subtitle: "Product Designer",
+          image: "assets/kelas_flutter.png",
           id: 1,
           progress: 60),
       LearnModel(
@@ -30,18 +31,25 @@ class LearnWidget extends StatelessWidget {
               Container(
                 width: (lebar / 2) - 10,
                 height: tinggi / 6.5,
-                decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.circular(15)),
+                decoration: dataLoop.image != null
+                    ? BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(dataLoop.image!),
+                            fit: BoxFit.cover),
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(15))
+                    : BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(15)),
               ),
               const SizedBox(
                 height: 5,
               ),
-              Text(dataLoop.subtitle!),
+              Text(dataLoop.subtitle),
               const SizedBox(
                 height: 5,
               ),
-              Text(dataLoop.title!),
+              Text(dataLoop.title),
               const SizedBox(
                 height: 5,
               ),
@@ -64,13 +72,14 @@ class LearnWidget extends StatelessWidget {
 }
 
 class LearnModel {
-  String? title, subtitle;
+  late String title, subtitle;
   late int id, progress;
-
+  String? image;
   LearnModel({
-    this.title,
+    required this.title,
     required this.subtitle,
     required this.id,
+    this.image,
     required this.progress,
   });
 }
